5b1a234544e51e9d061cedaf6f1829aa2fe8cc05
Tom <tom31@hotmail.co.uk>
2021-09-15 01:48:54 -0400

    fixed numeric path name

--- IterateQ4.bat
+++ IterateQ4.bat
@@ -7,13 +7,13 @@ set MODPATH=%1
 set DLLPATH=%2
 
 rem If your 7zip path is different add it to the PATH manually or change this
-set 7ZPATH=C:\Program Files\7-Zip
+set ZPATH=C:\Program Files\7-Zip
 
 rem check if 7zip is on path if not add it
 where /q 7z
 IF ERRORLEVEL 1 (
-   ECHO adding 7zip to PATH
-   set PATH=%PATH%;C:\Program Files\7-Zip;
+   ECHO Adding 7-zip to PATH
+   set PATH=%PATH%;%ZPATH%;
    )
 
 
