2cf4d745ff09fddace91889b13c224d64b825bab
Tom <tom31@hotmail.co.uk>
2021-09-15 03:24:43 -0400

    added quake4 auto launch

--- IterateQ4.bat
+++ IterateQ4.bat
@@ -1,13 +1,22 @@
-echo off
-rem script to copy quake4 game dll into pak file
+@echo off
+rem script to copy quake4 game dll into pak file and launch mod 
 rem Thomas Garside Sept 2021
 
 rem take variables from path 
 set MODPATH=%1
 set DLLPATH=%2
 
+rem split mod folder from MODPATH
+for %%f in (%MODPATH%) do set MODFOLDER=%%~nxf
+echo %MODFOLDER%
+
+rem save launch dir so you can return(q4 needs pwd to be its folder)
+set LAUNCHDIR=%CD%
+
+
 rem If your 7zip path is different add it to the PATH manually or change this
 set ZPATH=C:\Program Files\7-Zip
+
 
 rem check if 7zip is on path if not add it
 where /q 7z
@@ -24,3 +33,10 @@ rm %MODPATH%\Gamex86.dll
 
 rem Add new Gamex86.dll to PAK 
 7z a %MODPATH%\game000.pk4 %DLLPATH% 
+  
+rem launch quake4 mod directly with launch video disabled 
+cd %MODPATH%\..
+Quake4 +disconnect +set fs_game %MODFOLDER%
+
+rem return to directory you started in 
+cd %LAUNCHDIR%
